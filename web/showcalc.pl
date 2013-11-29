d849b6a808d4ebd3cbd690d5ccd4f58b3037640f
Michael Sauer <sauer.uetersen@googlemail.com>
2013-11-29 10:35:15 +0100

    Added a Nr Row to Webinterface

--- web/showcalc.pl
+++ web/showcalc.pl
@@ -15,14 +15,16 @@ my $dbh = DBI->connect($dsn, $sql_user, $sql_pass);
 
 sub show_calc() {
 	print h1('ninbot Calc DB Editor');
-	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='20%'>Name</TD><TD WIDTH='70%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
+	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='5%'>Nr.</TD><TD width='20%'>Name</TD><TD WIDTH='65%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
 	my $sth = $dbh->prepare("SELECT * FROM calc WHERE name NOT REGEXP '(^data-)|(^com-)'");
 	if ( $sth->execute ) {
+		my $counter = 1;
 		while ( my @row = $sth->fetchrow_array() ) {
 			if ( !defined $row[3] ) { $row[3] = ""; }
 			if ( !defined $row[4] ) { $row[4] = ""; }
 			if ( !defined $row[5] ) { $row[5] = ""; }
-			print "\t<TR><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+			print "\t<TR><TD>".$counter."</TD><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+			$counter++;
 		}
 	}
 	$sth->finish();
@@ -31,15 +33,17 @@ sub show_calc() {
 
 sub show_data() {
 	print h1('ninbot Data Variable DB Editor');
-	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='20%'>Name</TD><TD WIDTH='70%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
+	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='5%'>Nr.</TD><TD width='20%'>Name</TD><TD WIDTH='65%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
 	my $sth = $dbh->prepare("SELECT * FROM calc WHERE name REGEXP '^data-'");
 	if ( $sth->execute ) {
+		my $counter = 1;
 		while ( my @row = $sth->fetchrow_array() ) {
 			if ( !defined $row[3] ) { $row[3] = ""; }
 			if ( !defined $row[4] ) { $row[4] = ""; }
 			if ( !defined $row[5] ) { $row[5] = ""; }
 			if ($row[0] !~ m/^data-.?var/i) {
-				print "\t<TR><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+				print "\t<TR><TD>".$counter."</TD><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+				$counter++;
 			}
 		}
 	}
@@ -48,14 +52,16 @@ sub show_data() {
 
 sub show_nvar() {
 	print h1('ninbot Data Variable DB Editor');
-	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='20%'>Name</TD><TD WIDTH='70%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
+	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='5%'>Nr.</TD><TD width='20%'>Name</TD><TD WIDTH='65%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
 	my $sth = $dbh->prepare("SELECT * FROM calc WHERE name REGEXP '^data-nvar-'");
 	if ( $sth->execute ) {
+		my $counter = 1;
 		while ( my @row = $sth->fetchrow_array() ) {
 			if ( !defined $row[3] ) { $row[3] = ""; }
 			if ( !defined $row[4] ) { $row[4] = ""; }
 			if ( !defined $row[5] ) { $row[5] = ""; }
-			print "\t<TR><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+			print "\t<TR><TD>".$counter."</TD><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+			$counter++;
 		}
 	}
 	print "</TABLE>";
@@ -63,15 +69,16 @@ sub show_nvar() {
 
 sub show_var() {
 	print h1('ninbot Data Variable DB Editor');
-	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='20%'>Name</TD><TD WIDTH='70%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
+	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='5%'>Nr.</TD><TD width='20%'>Name</TD><TD WIDTH='65%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
 	my $sth = $dbh->prepare("SELECT * FROM calc WHERE name REGEXP '^data-var-'");
 	if ( $sth->execute ) {
+		my $counter = 1;
 		while ( my @row = $sth->fetchrow_array() ) {
 			if ( !defined $row[3] ) { $row[3] = ""; }
 			if ( !defined $row[4] ) { $row[4] = ""; }
 			if ( !defined $row[5] ) { $row[5] = ""; }
 			if ($row[1] =~ m/@@/i) {
-				print "\t<TR><TD>".$row[0]."</TD><TD>";
+				print "\t<TR><TD>".$counter."</TD><TD>".$row[0]."</TD><TD>";
 				print "<select size='3'>\n";
 				my @arr = split(/@@/, $row[1]);
 				foreach my $entry (@arr) {
@@ -80,8 +87,9 @@ sub show_var() {
 				print "</select>\n";
 				print "</TD><TD>".$row[2]."</TD></TR>\n";
 			} else {
-				print "\t<TR><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+				print "\t<TR><TD>".$counter."</TD><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
 			}
+			$counter++;
 		}
 	}
 	print "</TABLE>";
@@ -89,14 +97,16 @@ sub show_var() {
 
 sub show_com() {
 	print h1('ninbot Command DB Editor');
-	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='20%'>Name</TD><TD WIDTH='70%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
+	print "<TABLE width='100%' BORDER='1'>\n\t<TR><TD width='5%'>Nr.</TD><TD width='20%'>Name</TD><TD WIDTH='65%'>Inhalt</TD><TD WIDTH='10%'>Author</TD></TR>\n";
 	my $sth = $dbh->prepare("SELECT * FROM calc WHERE name REGEXP '^com-'");
 	if ( $sth->execute ) {
+		my $counter = 1;
 		while ( my @row = $sth->fetchrow_array() ) {
 			if ( !defined $row[3] ) { $row[3] = ""; }
 			if ( !defined $row[4] ) { $row[4] = ""; }
 			if ( !defined $row[5] ) { $row[5] = ""; }
-			print "\t<TR><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+			print "\t<TR><TD>".$counter."</TD><TD>".$row[0]."</TD><TD>".$row[1]."</TD><TD>".$row[2]."</TD></TR>\n";
+			$counter++;
 		}
 	}
 	print "</TABLE>";
