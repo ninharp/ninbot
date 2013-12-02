4f0f0419be791d13f84e21340645dd61dc4c733f
Michael Sauer <sauer.uetersen@googlemail.com>
2013-12-02 16:41:07 +0100

    Added version.sh that i recently used as post-commit hook for git but it isnt practicable as git hook so i created a manual version increment script that you can add to your IDE to increment version at compile time

---
+++ version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DIR=/home/michael/workspace/ninbot
+
+VER_FILE=$DIR/VERSION
+VERSION=$(cat $VER_FILE)
+echo $DIR
+F_VER=$(echo $VERSION |cut -d'.' -f1)
+S_VER=$(echo $VERSION |cut -d'.' -f2)
+SUBVER=$(echo $VERSION |cut -d'.' -f3)
+SUBVER=$(( $SUBVER + 1 ))
+echo $F_VER.$S_VER.$SUBVER > $VER_FILE
