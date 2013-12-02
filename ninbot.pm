2d937bda6fa2654aeee5e2dde59d57906afc661b
Michael Sauer <sauer.uetersen@googlemail.com>
2013-12-02 13:48:32 +0100

    Fixed a calc_level issue in ninbot.pm

--- ninbot.pm
+++ ninbot.pm
@@ -613,6 +613,7 @@ sub IRC_on_public {
             my @calc = $calc_self->get_Calc( "com-" . $command );
             if ( defined $calc[2] ) {
                 my ( $nr, $calc_name, $calc_text, $calc_nick, $calc_date, $calc_changed, $calc_mode, $calc_flag, $calc_level ) = @calc;
+                $calc_level = 0 if (!defined $calc_level);
                 $self->log( 4, "<Main:IRC:pub> Found Calc Command for '$command'" );
                 if ( $calc_text =~ m/\{(.*)\}/i ) {
                     my $script = $1;
